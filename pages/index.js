7bba32a1c8c2aada3edb93a5e3a160eb21f47fab
FitFingers <hooper.j.1991@gmail.com>
2021-08-30 20:49:30 +0200

    feat: first styling

--- pages/index.js
+++ pages/index.js
@@ -1,66 +1,75 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "styles/Home.module.css";
+import Link from "next/link";
+import classes from "styles/Home.module.css";
+import MushroomBanner from "public/mushroom-banner.png";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className={classes.container}>
       <Head>
         <title>Satoshi Shrooms</title>
         <meta name="description" content="Satoshi Shrooms" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Satoshi Shrooms</h1>
+      <main className={classes.main}>
+        <div className={classes.fullWidth}>
+          <Image
+            alt="Satoshi Shrooms banner image"
+            src={MushroomBanner}
+            placeholder="blur"
+            className={classes.bannerImage}
+          />
+          <div className={classes.titleWrapper}>
+            <h1 className={classes.title}>Satoshi Shrooms</h1>
+            <p className={classes.description}>TestNet Implementation</p>
+          </div>
+        </div>
 
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <div className={classes.grid}>
+          <div className={classes.card}>
+            <div className={classes.cardBg}>
+              <Link passHref href="/about">
+                <div className={classes.cardInner}>
+                  <h2>Mint &rarr;</h2>
+                  <p>Purchase your own, brand-new Satoshi Shroom</p>
+                </div>
+              </Link>
+            </div>
+          </div>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <div className={classes.card}>
+            <div className={classes.cardBg}>
+              <Link passHref href="/about">
+                <div className={classes.cardInner}>
+                  <h2>Gallery &rarr;</h2>
+                  <p>View all the Satoshi Shrooms that were minted already</p>
+                </div>
+              </Link>
+            </div>
+          </div>
         </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
+      <footer className={classes.footer}>
+        <Link
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+          <>
+            Powered by{" "}
+            <span className={classes.logo}>
+              <Image
+                src="/vercel.svg"
+                alt="Vercel Logo"
+                width={72}
+                height={16}
+              />
+            </span>
+          </>
+        </Link>
       </footer>
     </div>
   );
