51688f8d1f9bd519089a68f04cf8f5fdb772fe10
FitFingers <hooper.j.1991@gmail.com>
2021-08-30 23:39:47 +0200

    feat: wip web3 integration

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,53 @@ import Image from "next/image";
 import Link from "next/link";
 import classes from "styles/Home.module.css";
 import MushroomBanner from "public/mushroom-banner.png";
+import { useCallback, useEffect, useState } from "react";
+
+// ===================================================
+// METAMASK
+// ===================================================
+
+import Web3 from "web3";
+
+function useMetaMask() {
+  const [account, setAccount] = useState(null);
+
+  useEffect(() => {
+    async function connectWallet() {
+      try {
+        window.web3 = new Web3(window.ethereum);
+        const accounts = await window.web3.eth.requestAccounts(); // connect to wallet
+        const network = await window.web3.eth.net.getChainId(); // 0x4 is Rinkeby
+        console.log("DEBUG web3", { network, accounts });
+      } catch (err) {
+        console.debug("ERROR: couldn't initialise web3", { err });
+      }
+    }
+    connectWallet();
+  }, []);
+
+  return { account };
+}
+
+// ===================================================
+// ===================================================
 
 export default function Home() {
+  const [mintCount, setMintCount] = useState(1);
+  const handleClickLess = useCallback(() => setMintCount((c) => c - 1), []);
+  const handleClickMore = useCallback(() => setMintCount((c) => c + 1), []);
+
+  const mint = useCallback(() => {
+    console.log("DEBUG", { mintCount });
+  }, [mintCount]);
+
+  const { account } = useMetaMask();
+
+  // TODO: REMOVE => DEBUG ONLY
+  useEffect(() => {
+    console.log("DEBUG metamask", { account });
+  }, [account]);
+
   return (
     <div className={classes.container}>
       <Head>
@@ -49,7 +94,9 @@ export default function Home() {
                   <a href="">
                     <div className={classes.cardInner}>
                       <h2>Gallery &rarr;</h2>
-                      <p>View all the Satoshi Shrooms that were minted already</p>
+                      <p>
+                        View all the Satoshi Shrooms that were minted already
+                      </p>
                     </div>
                   </a>
                 </Link>
@@ -68,7 +115,26 @@ export default function Home() {
                 Just connect your metamask, select the number to mint and click
                 the button!
               </p>
-              <button className={classes.button}><span>Mint</span></button>
+
+              <div className={classes.counter}>
+                <div
+                  className={[classes.less, classes.counterButton].join(" ")}
+                  onClick={handleClickLess}
+                >
+                  -
+                </div>
+                <div className={classes.display}>{mintCount}</div>
+                <div
+                  className={[classes.more, classes.counterButton].join(" ")}
+                  onClick={handleClickMore}
+                >
+                  +
+                </div>
+              </div>
+
+              <button className={classes.button}>
+                <span>Mint</span>
+              </button>
             </div>
             <div className={classes.tile}>
               <h3>Here you can mint your own Satoshi Shroom</h3>
