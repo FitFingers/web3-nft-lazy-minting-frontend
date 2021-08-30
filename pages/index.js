965b8b7357f1ef8ae9905a4db286d803b5d65c00
FitFingers <hooper.j.1991@gmail.com>
2021-08-30 22:35:36 +0200

    feat: styling

--- pages/index.js
+++ pages/index.js
@@ -14,43 +14,82 @@ export default function Home() {
       </Head>
 
       <main className={classes.main}>
+        {/* BANNER */}
         <div className={classes.fullWidth}>
-          {/* <div className={classes.bannerImage}> */}
-            <Image
-              alt="Satoshi Shrooms banner image"
-              src={MushroomBanner}
-              placeholder="blur"
-              // objectFit="cover"
-              // layout="fill"
-              className={classes.bannerImage}
-            />
-          {/* </div> */}
+          <Image
+            alt="Satoshi Shrooms banner image"
+            src={MushroomBanner}
+            placeholder="blur"
+            className={classes.bannerImage}
+          />
           <div className={classes.titleWrapper}>
             <h1 className={classes.title}>Satoshi Shrooms</h1>
             <p className={classes.description}>TestNet Implementation</p>
           </div>
         </div>
 
-        <div className={classes.grid}>
-          <div className={classes.card}>
-            <div className={classes.cardBg}>
-              <Link passHref href="/about">
-                <div className={classes.cardInner}>
-                  <h2>Mint &rarr;</h2>
-                  <p>Purchase your own, brand-new Satoshi Shroom</p>
-                </div>
-              </Link>
+        {/* BUTTON GRID */}
+        <div className={classes.buttons} id="buttons">
+          <div className={classes.grid}>
+            <div className={classes.card}>
+              <div className={classes.cardBg}>
+                <Link passHref href="#mint">
+                  <a href="">
+                    <div className={classes.cardInner}>
+                      <h2>Mint &rarr;</h2>
+                      <p>Purchase your own, brand-new Satoshi Shroom</p>
+                    </div>
+                  </a>
+                </Link>
+              </div>
+            </div>
+            <div className={classes.card}>
+              <div className={classes.cardBg}>
+                <Link passHref href="#gallery">
+                  <a href="">
+                    <div className={classes.cardInner}>
+                      <h2>Gallery &rarr;</h2>
+                      <p>View all the Satoshi Shrooms that were minted already</p>
+                    </div>
+                  </a>
+                </Link>
+              </div>
             </div>
           </div>
+        </div>
 
-          <div className={classes.card}>
-            <div className={classes.cardBg}>
-              <Link passHref href="/about">
-                <div className={classes.cardInner}>
-                  <h2>Gallery &rarr;</h2>
-                  <p>View all the Satoshi Shrooms that were minted already</p>
-                </div>
-              </Link>
+        {/* MINTING */}
+        <div className={classes.minting} id="mint">
+          <h2>Mint</h2>
+          <div className={classes.mintingInner}>
+            <div className={classes.tile}>
+              <h3>Here you can mint your own Satoshi Shroom</h3>
+              <p>
+                Just connect your metamask, select the number to mint and click
+                the button!
+              </p>
+              <button className={classes.button}><span>Mint</span></button>
+            </div>
+            <div className={classes.tile}>
+              <h3>Here you can mint your own Satoshi Shroom</h3>
+              <p>
+                Just connect your metamask, select the number to mint and click
+                the button!
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* GALLERY */}
+        <div className={classes.gallery} id="gallery">
+          <h2>Gallery</h2>
+          <div className={classes.galleryInner}>
+            <div className={classes.fullWidth}>
+              <Image
+                alt="Satoshi Shrooms banner image"
+                src={MushroomBanner}
+                placeholder="blur"
+              />
             </div>
           </div>
         </div>
@@ -58,7 +97,9 @@ export default function Home() {
 
       <footer className={classes.footer}>
         <Link passHref href="/">
-          <p>Satoshi Shrooms {new Date().getFullYear()}</p>
+          <a href="">
+            <p>Satoshi Shrooms {new Date().getFullYear()}</p>
+          </a>
         </Link>
       </footer>
     </div>
